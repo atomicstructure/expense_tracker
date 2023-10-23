9ee9fedab24f34d9aa076f862db1480c7ea6163f
Ogundare Oluwafemi <ogundarearnold@gmail.com>
2023-10-23 11:56:58 +0100

    Switching between device orientation to adjust App to screen size

--- expense_tracker/lib/widgets/expense.dart
+++ expense_tracker/lib/widgets/expense.dart
@@ -66,6 +66,8 @@ class _ExpensesState extends State<Expenses> {
 
   @override
   Widget build(BuildContext context) {
+    final width = MediaQuery.of(context).size.width;
+
     Widget mainContent = const Center(
       child: Text('No expenses added. Please add an expense!'),
     );
@@ -88,14 +90,23 @@ class _ExpensesState extends State<Expenses> {
           ),
         ],
       ),
-      body: Column(
-        children: [
-          Chart(expenses: _registeredExpenses),
-          Expanded(
-            child: mainContent,
-          )
-        ],
-      ),
+      body: width < 600
+          ? Column(
+              children: [
+                Chart(expenses: _registeredExpenses),
+                Expanded(
+                  child: mainContent,
+                )
+              ],
+            )
+          : Row(
+              children: [
+                Expanded(child: Chart(expenses: _registeredExpenses)),
+                Expanded(
+                  child: mainContent,
+                ),
+              ],
+            ),
     );
   }
 }
